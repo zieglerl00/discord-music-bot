85d15b7cdb097b0ddc1f050f4c754a3c3aba0769
zieglerl00 <lukas.ziegler52@gmail.com>
2022-12-06 18:51:53 +0100

    Initial

---
+++ main.py
@@ -0,0 +1,20 @@
+import discord
+from discord.ext import commands
+import os
+
+#import all of the cogs
+from help_cog import help_cog
+from music_cog import music_cog
+
+bot = commands.Bot(command_prefix='-')
+
+#remove the default help command so that we can write out own
+bot.remove_command('help')
+
+#register the class with the bot
+bot.add_cog(help_cog(bot))
+bot.add_cog(music_cog(bot))
+
+#start the bot with our token
+# bot.run("MTA0MjEyMzI5ODEwNTk4NzA3Ng.GUIkMN.FbuR6c25G1wu4cQOWp2LXyK7O9Guf4Q62zXy3g")
+bot.run("MTA0OTc0MTAwMjY4NjA4MzEzMw.GJr_9G.7iGqKgP2EyPWprjtpkaMt7I77wS8x6d-9ICPYg")
